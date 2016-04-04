880c5fbb11fdcc58c8f55f17cd335e21a2ce9133
Jadie <xz2467@columbia.edu>
2016-04-04 12:59:02 -0400

    update

--- lib/train.R
+++ lib/train.R
@@ -5,17 +5,13 @@ train <- function(dat_train, label_train, par=NULL){
 ## Tree Classification
 
 library(rpart)
+library(kernlab)
 tree.data <- data.frame(dat_train)
 adv <- rpart(label_train ~.,tree.data,method = "class",
               control = rpart.control(minsplit=30, cp=.005))
 
-
-  
-library(kernlab)
-# train the SVM
 baseline <- ksvm(x=dat_train,y=label_train,type="C-svc",kernel='vanilladot',C=par$depth,scaled=c())
-return(baseline)
-return(adv)
+return(list(baseline, adv))
 #printcp(tree)
 #summary(tree)
 
